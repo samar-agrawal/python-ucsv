d0a9f0bd9a6a4951869c5941cc27088a0eb71902
hzarka <hzarka@gmail.com>
2012-05-28 00:15:32 +0300

    .

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='ucsv',
     version='0.1',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
+    packages = find_packages('.'),
+    package_dir = {'': '.'},
     zip_safe=True,
 )
 
